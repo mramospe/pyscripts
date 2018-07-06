ddca3b77eb6a6d1aed2ae816e29402bd56051548
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2018-07-06 19:05:53 +0200

    Define version 0.0.0.dev3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages, Extension
 # for the fourth tag are "dev", "alpha", "beta",
 # "cand", "final" or "post".
 #
-version_info = (0, 0, 0, 'dev', 2)
+version_info = (0, 0, 0, 'dev', 3)
 
 tag = version_info[3]
 
