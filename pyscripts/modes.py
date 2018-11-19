c14db1d408747a57e9853737797ae0cae3b0294c
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2018-11-20 00:14:42 +0100

    Add decorator 'extends'. Remove one underscore in attributes '__kwargs' and '__func' of class Mode. Do not use 'wraps' from functools module since it is meant to be used with functions.

--- pyscripts/modes.py
+++ pyscripts/modes.py
@@ -5,11 +5,48 @@ Classes and functions to define modes in scripts.
 __author__  = ['Miguel Ramos Pernas']
 __email__   = ['miguel.ramos.pernas@cern.ch']
 
-# Python
-from functools import wraps
+
+__all__ = ['extends', 'mode', 'Mode']
 
 
-__all__ = ['mode', 'Mode']
+def extends( modes, collision_policy=None ):
+    '''
+    Function to decorate a mode so it also expects the inputs of those
+    specified in "modes".
+    The argument "collision_policy" defines what to do in case two or
+    more modes have the same arguments.
+    It must be a function taking two arguments.
+    One is the current value of the argument, while the other is that
+    from the new mode being processed.
+    The value of the argument is set to its output.
+    By default, if two or more modes expect the same argument and the
+    value is different for any of them, a :class:`RuntimeError`
+    is raised.
+
+    :param modes: modes to extend the decorated function with.
+    :type modes: container(Mode)
+    :param collision_policy: function defining the policy to use when two \
+    or more modes expect the same arguments.
+    :type collision_policy: function
+    '''
+    collision_policy = collision_policy or {}
+
+    kwargs = {}
+    for m in modes:
+
+        for k, v in m._kwargs.items():
+
+            if k in kwargs:
+
+                if k in collision_policy:
+                    kwargs[k] = collision_policy[k](kwargs[k], v)
+                elif kwargs[k] != v:
+                    raise RuntimeError('Collision for key "{}", and no policy has been specified'.format(k))
+
+            else:
+                kwargs[k] = v
+
+    return mode(**kwargs)
 
 
 def mode( *args, **kwargs ):
@@ -25,20 +62,20 @@ def mode( *args, **kwargs ):
     '''
     err = ValueError('Function "mode" can only be called either taking a function or a set of keyword arguments')
 
-    if len(args) == 1 is not None:
+    if len(args) == 1:
 
         if kwargs:
             raise err
 
         f = args[0]
 
-        return wraps(f)(Mode(f, kwargs))
+        return Mode(f, kwargs)
 
     elif len(args) > 1:
         raise err
     else:
         def decorator( func ):
-            return wraps(func)(Mode(func, kwargs))
+            return Mode(func, kwargs)
 
         return decorator
 
@@ -49,10 +86,9 @@ class Mode(object):
         '''
         Represent a mode to be run on a script.
         To correctly decorate a function, use :func:`pyscripts.mode` instead.
-        It holds a name, which by default is set to that of the function,
-        and the information provided in "kwargs", which can be accessed
-        either by index (using the method :method:`pyscripts.Mode.__getitem__`),
-        or as an attribute:
+        It holds the function and the information provided in "kwargs", which
+        can be accessed either by index (using the method
+        :method:`pyscripts.Mode.__getitem__`), or as an attribute:
 
         .. code-block:: python
 
@@ -67,6 +103,13 @@ class Mode(object):
            >>> foo.inputs
            {'input_file': 'file.txt'}
 
+        Both the name of the class and the docstring are changed to match that
+        of the function.
+        Note that the result of :python:`help(mode)` will still show the
+        information about the :class:`pyscripts.Mode` class.
+        These variables are stored so one can acccess the information of how
+        to execute the function.
+
         :param func: function to call.
         :type func: function
         :param kwargs: dictionary holding the different arguments that will \
@@ -77,8 +120,12 @@ class Mode(object):
         '''
         super(Mode, self).__init__()
 
-        self.__func   = func
-        self.__kwargs = kwargs
+        self._func   = func
+        self._kwargs = kwargs
+
+        # Store the name and change the docstring.
+        self.__name__ = func.__name__
+        self.__doc__  = func.__doc__
 
     def __call__( self, *args, **kwargs ):
         '''
@@ -87,13 +134,13 @@ class Mode(object):
 
         :returns: whatever the wrapped function returns.
         '''
-        cm = set(self.__kwargs.keys()).intersection(kwargs.keys())
+        cm = set(self._kwargs.keys()).intersection(kwargs.keys())
         if cm:
             raise ValueError('Colliding keyword arguments: {}'.format(cm))
 
-        kwargs.update(self.__kwargs)
+        kwargs.update(self._kwargs)
 
-        return self.__func(*args, **kwargs)
+        return self._func(*args, **kwargs)
 
     def __getattr__( self, arg ):
         '''
@@ -115,4 +162,4 @@ class Mode(object):
         :type arg: str
         :returns: value of the argument passed to the function.
         '''
-        return self.__kwargs[arg]
+        return self._kwargs[arg]
--- tests/test_modes.py
+++ tests/test_modes.py
@@ -13,44 +13,115 @@ import pytest
 import pyscripts
 
 
+def test_extends():
+    '''
+    Test for the "extends" decorator.
+    '''
+    # Define decorated "modes"
+    @pyscripts.mode( p1 = 'p1' )
+    def func1( p1, other ):
+        assert p1 == 'p1'
+
+    @pyscripts.mode
+    def func2( p2 ):
+        pass
+
+    @pyscripts.mode( p3 = 'p3' )
+    def func3( p3 ):
+        assert p3 == 'p3'
+
+    @pyscripts.mode(inputs={'file1': 'path/to/file1.txt'})
+    def func4( inputs ):
+        assert 'file1' in inputs
+
+    @pyscripts.mode(inputs={'file2': 'path/to/file2.txt'})
+    def func5( inputs ):
+        assert 'file2' in inputs
+
+    @pyscripts.mode(inputs={'file2': 'path/to/file2.txt'})
+    def func6( inputs ):
+        assert 'file2' in inputs
+        assert inputs['file2'] == 'path/to/file2.txt'
+
+    @pyscripts.mode(inputs={'file2': 'colliding/file2.txt'})
+    def func7( inputs ):
+        assert 'file2' in inputs
+        assert inputs['file2'] == 'colliding/file2.txt'
+
+    # Define the main decorated mode which depends on the previous ones
+    @pyscripts.mode(main_par='main')
+    @pyscripts.extends([func1, func2, func3, func5, func6],
+                       collision_policy={'inputs': lambda a, b: {**a, **b}})
+    def main( p1, p3, inputs, main_par ):
+        ''' This is the docstring for "main". '''
+        func1(other='other')
+        func2('takes nothing')
+
+        for f in (func3, func5, func6):
+            f()
+
+        assert main_par == 'main'
+
+    # Run the function
+    main()
+
+    # Check name and docstring of the function
+    assert main.__name__ == 'main'
+    assert main.__doc__  == ''' This is the docstring for "main". '''
+
+    # Test the breaking situations
+    with pytest.raises(RuntimeError):
+        @pyscripts.mode(main_par='main')
+        @pyscripts.extends([func5, func6, func7])
+        def main2( inputs, main_par ):
+            for f in (func5, func6, func7):
+                f()
+            assert main_par == 'main'
+
+
 def test_mode():
     '''
     Test for the "mode" function and the "Mode" class.
     '''
     @pyscripts.mode(inputs={'file': 'test.txt'})
-    def dummy_i( inputs ):
+    def func_i( inputs ):
+        ''' This is a mode with docstring '''
         assert inputs['file'] == 'test.txt'
 
     @pyscripts.mode(outputs={'file': 'test.txt'})
-    def dummy_o( outputs ):
+    def func_o( outputs ):
         assert outputs['file'] == 'test.txt'
 
     @pyscripts.mode
-    def dummy_n():
+    def func_n():
         pass
 
     @pyscripts.mode(inputs={'ifile': 'test_i.txt'}, outputs={'ofile': 'test_o.txt'})
-    def dummy_d( inputs, outputs ):
+    def func_d( inputs, outputs ):
         assert inputs['ifile'] == 'test_i.txt'
         assert outputs['ofile'] == 'test_o.txt'
 
     # Check all the functions
-    for f in (dummy_i, dummy_o, dummy_n, dummy_d):
+    for f in (func_i, func_o, func_n, func_d):
         f()
+
+    # Check that the name and docstrings change
+    assert func_i.__name__ == 'func_i'
+    assert func_i.__doc__  == ''' This is a mode with docstring '''
 
     # Check that the output of taking arguments by key is the same as by
     # attribute
-    for f in (dummy_i, dummy_d):
+    for f in (func_i, func_d):
         f.inputs is f['inputs']
 
-    for f in (dummy_o, dummy_d):
+    for f in (func_o, func_d):
         f.outputs is f['outputs']
 
     # Check that the names remain the same
-    assert dummy_i.__name__ == 'dummy_i'
-    assert dummy_n.__name__ == 'dummy_n'
+    assert func_i.__name__ == 'func_i'
+    assert func_n.__name__ == 'func_n'
 
     # If one calls the function with an argument that will already be given
     # by "mode", an error is raised.
     with pytest.raises(ValueError):
-        dummy_i(inputs=None)
+        func_i(inputs=None)
